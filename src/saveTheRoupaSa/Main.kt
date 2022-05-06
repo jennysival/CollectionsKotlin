56bb510cfcda94178334fdc2d0f4d8616025125b
jennysival <jeniffersival@gmail.com>
2022-05-06 14:24:57 -0300

    Limpar enunciado do main

--- src/saveTheRoupaSa/Main.kt
+++ src/saveTheRoupaSa/Main.kt
@@ -1,7 +1,5 @@
 package saveTheRoupaSa
 
-//No arquivo Main, criar um cenário em que alguém guarda
-//duas peças, recebe o código e depois retira suas peças.
 
 fun main() {
     println("Seja bem vinde ao guarda-volumes SaveTheRoupa S.A!")
@@ -9,8 +7,4 @@ fun main() {
     val novoMenu = Menu()
 
     novoMenu.guardarOuRetirar()
-
-
-
-
 }
